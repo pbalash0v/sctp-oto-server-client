bc04c3529dbd0375924a1224c1b5124a6523720f
Pavel Balashov <you@example.com>
2019-12-04 12:48:10 +0300

    IClient interface extracted

--- src/server/sctp_srvr/sctp_srvr_client.h
+++ src/server/sctp_srvr/sctp_srvr_client.h
@@ -5,45 +5,7 @@
 #include <usrsctp.h>
 
 #include "ssl_h.h"
-
-class SCTPServer;
-
-
-class IClient {
-public:
-   enum State {
-   	NONE,
-      SCTP_ACCEPTED,
-      SCTP_CONNECTED,
-      SSL_HANDSHAKING,
-      SSL_CONNECTED,
-		SSL_SHUTDOWN,
-		PURGE
-   };
-
-	IClient(struct socket* s, SCTPServer& srv)
-	 : sock(s), server_(srv) {};
-
-	virtual ~IClient() {};
-
-	virtual void init() = 0;
-	virtual void set_state(IClient::State) = 0;
-	virtual std::string to_string() const = 0;
-
-	struct socket* sock = nullptr;
-
-	SCTPServer& server_;
-
-	State state = NONE;
-
-	SSL* ssl = nullptr;
-	BIO* output_bio = nullptr;
-	BIO* input_bio = nullptr;
-	
-	friend std::ostream& operator<<(std::ostream &out, const IClient::State s);
-	friend class SCTPServer;
-};
-
+#include "sctp_srvr_i_client.h"
 
 
 class Client : public IClient {
@@ -56,11 +18,11 @@ public:
 
 	virtual ~Client();
 
-	virtual void init();
+	virtual void init() override;
 
-	virtual void set_state(Client::State new_state);
+	virtual void set_state(Client::State new_state) override;
 
-	virtual std::string to_string() const;
+	virtual std::string to_string() const override;
 
 	friend std::ostream& operator<<(std::ostream &out, const Client &c);
 
---
+++ src/server/sctp_srvr/sctp_srvr_i_client.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+
+#include <usrsctp.h>
+
+#include "ssl_h.h"
+
+
+class SCTPServer;
+
+class IClient {
+public:
+   enum State {
+   	NONE,
+      SCTP_ACCEPTED,
+      SCTP_CONNECTED,
+      SSL_HANDSHAKING,
+      SSL_CONNECTED,
+		SSL_SHUTDOWN,
+		PURGE
+   };
+
+	IClient(struct socket* s, SCTPServer& srv)
+	 : sock(s), server_(srv) {};
+
+	virtual ~IClient() {};
+
+	virtual void init() = 0;
+	virtual void set_state(IClient::State) = 0;
+	virtual std::string to_string() const = 0;
+
+	struct socket* sock = nullptr;
+
+	SCTPServer& server_;
+
+	State state = NONE;
+
+	SSL* ssl = nullptr;
+	BIO* output_bio = nullptr;
+	BIO* input_bio = nullptr;
+	
+	friend std::ostream& operator<<(std::ostream &out, const IClient::State s);
+	friend class SCTPServer;
+};
+
+
