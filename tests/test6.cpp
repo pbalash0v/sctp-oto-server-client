22aacdbbe2b3f1859f41921f565b7fc72b210282
Pavel Balashov <you@example.com>
2019-12-20 14:28:56 +0300

    SyncQueue test

---
+++ tests/test6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+#include "sync_queue.hpp"
+
+
+int main(int, char const**) {
+	constexpr auto Q_SIZE = 50;	
+	constexpr auto Q_LIMIT = 10;
+
+	{
+		SyncQueue<int> q;
+		for (int i = 0; i < Q_SIZE; ++i) q.enqueue(i);
+		assert(q.size() == Q_SIZE);
+		q.dequeue();
+		assert(q.size() == Q_SIZE-1);
+	}
+
+
+	{
+		SyncQueue<int> q(true, Q_LIMIT);
+		for (int i = 0; i < Q_SIZE; ++i) q.enqueue(i);
+		assert(q.size() == Q_LIMIT);
+		q.dequeue();
+		assert(q.size() == Q_LIMIT-1);
+	}
+
+
+	return EXIT_SUCCESS;
+}
